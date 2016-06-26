b3c1e8797aadce6da4a1c0b624cc2391a0a91b52
cxm <cxm@cxm-PC>
2016-06-26 11:03:15 +0800

    TwoSum

---
+++ src/LeetCode/TwoSum.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+public class TwoSum {
+	public static void main(String[] args) {
+		System.out.println("aaa");
+	}
+	
+	public static int[] twoSum(int[] nums, int target) {
+        return null;
+	}
+}
