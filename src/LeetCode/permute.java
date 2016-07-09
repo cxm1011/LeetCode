63ccbfaeb4e07c76281465204665660025e95f5f
cxm <cxm@58.198.82.190>
2016-07-09 10:17:04 +0800

    46. Permutations  -- beats 39.89%

---
+++ src/LeetCode/permute.java
@@ -0,0 +1,37 @@
+package LeetCode;
+import java.util.*;
+public class permute {
+	public static void main(String args[])
+	{
+		int[] nums={2,3,1};
+		List<List<Integer>> ans= permute(nums);
+		System.out.println(ans);
+	}
+	public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if(nums.length == 0) return ans;
+        List<Integer> temp = new ArrayList<Integer>();
+        int length = nums.length;
+        ans = find(ans,temp,nums,length);
+        return ans;
+    }
+	public static List<List<Integer>> find(List<List<Integer>> ans,List<Integer> temp,int[] nums,int length)
+	{
+		if(temp.size() == length)
+		{
+			ans.add(new ArrayList<Integer>(temp));
+			return ans;
+		}
+		int i = 0;
+		for(i = 0;i<length;i++)
+		{
+			if(temp.contains(nums[i]))
+				continue;
+			temp.add(nums[i]);
+		    ans = find(ans,temp,nums,length);
+		    temp.remove(temp.size()-1);
+		}
+		return ans;
+	}
+
+}
